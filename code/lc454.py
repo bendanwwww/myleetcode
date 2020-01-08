2fa836ab4589a219921e3f7f3ab634ef5448d00a
liushuoyang <liushuoyang@100tal.com>
2020-01-08 18:18:53 +0800

    leetcode 454

---
+++ code/lc454.py
@@ -0,0 +1,59 @@
+"""
+给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] + D[l] = 0。
+
+为了使问题简单化，所有的 A, B, C, D 具有相同的长度 N，且 0 ≤ N ≤ 500 。所有整数的范围在 -2^28 到 2^28 - 1 之间，最终结果不会超过 2^31 - 1 。
+
+例如:
+
+输入:
+A = [ 1, 2]
+B = [-2,-1]
+C = [-1, 2]
+D = [ 0, 2]
+
+输出:
+2
+
+解释:
+两个元组如下:
+1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+
+"""
+
+class Solution(object):
+    def fourSumCount(self, A, B, C, D):
+        map1 = {}
+        map2 = {}
+        for i in range(len(A)):
+            for x in range(len(B)):
+                sum = A[i] + B[x]
+                if sum in map1:
+                    map1[sum] = map1[sum] + 1
+                else:
+                    map1[sum] = 1
+        for i in range(len(C)):
+            for x in range(len(D)):
+                sum = C[i] + D[x]
+                if sum in map2:
+                    map2[sum] = map2[sum] + 1
+                else:
+                    map2[sum] = 1
+
+        keys1 = list(map1.keys())
+        res = 0
+        for i in range(len(keys1)):
+            if -keys1[i] in map2:
+                res = res + map1[keys1[i]] * map2[-keys1[i]]
+        return res
+
+
+
+A = [1, 2]
+B = [-2, -1]
+C = [-1, 2]
+D = [0, 2]
+s = Solution()
+res = s.fourSumCount(A, B, C, D)
+print(res)
+
