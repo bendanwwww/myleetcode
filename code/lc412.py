ffaeeb7c43502557fe6c3ec692c5dc2fcffd2429
liushuoyang <liushuoyang@100tal.com>
2020-01-08 18:34:50 +0800

    leetcode 412

---
+++ code/lc412.py
@@ -0,0 +1,47 @@
+"""
+写一个程序，输出从 1 到 n 数字的字符串表示。
+1. 如果 n 是3的倍数，输出“Fizz”；
+2. 如果 n 是5的倍数，输出“Buzz”；
+3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
+
+示例：
+n = 15,
+
+返回:
+[
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"
+]
+
+"""
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        res = [''] * n
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                res[i - 1] = "FizzBuzz"
+            elif i % 3 == 0:
+                res[i - 1] = "Fizz"
+            elif i % 5 == 0:
+                res[i - 1] = "Buzz"
+            else:
+                res[i - 1] = str(i)
+        return res
+
+s = Solution()
+res = s.fizzBuzz(15)
+print(res)
