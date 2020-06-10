20d73ba8d3014fe4173f073e8fa4eb0078fecde0
liushuoyang <461806307@qq.com>
2020-06-10 23:51:55 +0800

    leetcode 66 69

---
+++ code/lc66.py
@@ -0,0 +1,35 @@
+"""
+给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+你可以假设除了整数 0 之外，这个整数不会以零开头。
+
+示例 1:
+输入: [1,2,3]
+输出: [1,2,4]
+解释: 输入数组表示数字 123。
+
+示例 2:
+输入: [4,3,2,1]
+输出: [4,3,2,2]
+解释: 输入数组表示数字 4321。
+
+"""
+
+class Solution(object):
+    def plusOne(self, digits):
+        b = True
+        for i in range(len(digits) - 1, -1, -1):
+            if b:
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    b = False
+        if b:
+            return [1] + digits
+        else:
+            return digits
+
+s = Solution()
+res = s.plusOne([9, 9, 9, 9])
+print(res)
