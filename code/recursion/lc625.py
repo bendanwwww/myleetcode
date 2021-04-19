075453c590335bbbd55b4c1e8a5064c5e4024c19
bendanwwww <461806307@qq.com>
2021-04-19 15:50:16 +0800

    leetcode 625

--- code/recursion/lc625.py
+++ code/recursion/lc625.py
@@ -1,4 +1,20 @@
+"""
+给定一个正整数 a，找出最小的正整数 b 使得 b 的所有数位相乘恰好等于 a。
+如果不存在这样的结果或者结果不是 32 位有符号整数，返回 0。
 
+样例 1
+输入：
+48
+输出：
+68
+ 
+样例 2
+输入：
+15
+输出：
+35
+
+"""
 
 class Solution(object):
     res = 0
@@ -7,7 +23,6 @@ class Solution(object):
         self.all_num_list = []
         if a < 10:
             return a
-        # for i in range(2, 9):
         self.find(a, 2, [])
         return self.res
 
@@ -29,7 +44,6 @@ class Solution(object):
                 tmp_list = num_list[:]
                 tmp_list.append(str(i))
                 self.find(a / i, i, tmp_list)
-                # break
 
 s = Solution()
 res = s.smallestFactorization(18000000)
